80290726e0557b8fb8c40b95c4bd83d413d19695
gbarbon <gianluca.barbon@gmail.com>
2015-08-03 18:11:22 +0200

    Added comments and getters on absyn.scala

--- tools/src/it/unive/dais/yaasa/absyn.scala
+++ tools/src/it/unive/dais/yaasa/absyn.scala
@@ -6,133 +6,257 @@ package it.unive.dais.yaasa.absyn
 
 import it.unive.dais.yaasa.utils.parsingUtils._
 
-class Node(loc: Location) {
+abstract class Node(loc: Location) {
   val location = loc
+
+  /**
+   * toString method
+   * @return
+   */
+  def pretty: String
+  //override def toString() = this.pretty()
 }
 
-class TaggedNode[T](loc: Location, _tag: T)
-    extends Node(loc) {
-  val tag = _tag
-}
-
-case class Class(name: String, ext: String, fields: List[FieldDecl], methods: List[MethodDecl]) {
-}
-
-case class FieldDecl(ty: Type, names: List[String]) {
-
-}
-
-case class MethodDecl(returnTy: Option[Type], name: String, formals: Option[List[Formal]], body: Block) {
-}
-
-case class Formal(ty: Type, name: String) {
-
-}
-
-abstract class Type(_name: String) {
+/**
+ *
+ */
+case class Class(_name: String, _ext: String, _fields: List[FieldDecl], _methods: List[MethodDecl], _loc: Location)
+    extends Node(_loc) {
   val name = _name
+  val ext = _ext
+  val fields = _fields
+
+  override def pretty = ""
 }
 
-case class TyInt()
-  extends Type("Int") {}
+case class FieldDecl(_ty: Type, _names: List[String], _loc: Location)
+    extends Node(_loc) {
+  val ty = _ty
+  val names = _names
 
-case class TyBool()
-  extends Type("Bool") {}
-
-case class TyString()
-  extends Type("String") {}
-
-case class TyType(_name: String)
-  extends Type(_name) {}
-
-case class Block(varDecls: List[VarDecl], stmts: List[Stmt]) {
-
+  override def pretty = ""
 }
 
-case class VarDecl(ty: Type, id: String) {
+case class MethodDecl(_returnTy: Option[Type], _name: String, _formals: Option[List[Formal]], _body: Block, _loc: Location)
+    extends Node(_loc) {
+  val returnTy = _returnTy
+  val name = _name
+  val formals = _formals
+  val body = _body
 
+  override def pretty = ""
 }
 
-abstract class Stmt() {}
+case class Formal(_ty: Type, _name: String, _loc: Location)
+    extends Node(_loc) {
+  val ty = _ty
+  val name = _name
 
-case class Skip() extends Stmt() {}
-
-case class Assign(name: String, value: Expr)
-  extends Stmt() {}
-
-case class SetField(loc: Field, value: Expr)
-  extends Stmt() {}
-
-case class StmtCall(name: String, actuals: List[Expr])
-  extends Stmt() {}
-
-case class MethodCall(loc: Field, actuals: List[Expr])
-  extends Stmt() {}
-
-case class Return(value: Option[Expr])
-  extends Stmt() {}
-
-case class If(cond: Expr, thn: Block, els: Block)
-  extends Stmt() {}
-
-case class While(cond: Expr, body: Block)
-  extends Stmt() {}
-
-case class Field(expr: Expr, name: String) {
+  override def pretty = ""
 }
 
-abstract class Expr() {
+abstract class Type(_name: String, _loc: Location)
+    extends Node(_loc) {
+  val name = _name
 
 }
 
-case class Id(name: String)
-    extends Expr() {
+case class TyInt(_loc: Location)
+    extends Type("Int", _loc) {
+
+  override def pretty = ""
+}
+
+case class TyBool(_loc: Location)
+    extends Type("Bool", _loc) {
+
+  override def pretty = ""
+}
+
+case class TyString(_loc: Location)
+    extends Type("String", _loc) {
+
+  override def pretty = ""
+}
+
+case class TyType(_name: String, _loc: Location)
+    extends Type(_name, _loc) {
+
+  override def pretty = ""
+}
+
+case class Block(_varDecls: List[VarDecl], _stmts: List[Stmt], _loc: Location)
+    extends Node(_loc) {
+
+  override def pretty = ""
+}
+
+case class VarDecl(_ty: Type, _id: String, _loc: Location)
+    extends Node(_loc) {
+  val ty = _ty
+  val id = _id
+
+  override def pretty = ""
+}
+
+abstract class Stmt(_loc: Location)
+  extends Node(_loc) {}
+
+case class Skip(_loc: Location)
+    extends Stmt(_loc) {
+
+  override def pretty = ""
+}
+
+case class Assign(_name: String, _value: Expr, _loc: Location)
+    extends Stmt(_loc) {
+  val name = _name
+  val value = _value
+
+  override def pretty = ""
+}
+
+case class SetField(_fi: Field, _value: Expr, _loc: Location)
+    extends Stmt(_loc) {
+  val fi = _fi
+  val value = _value
+
+  override def pretty = ""
+}
+
+case class StmtCall(_name: String, _actuals: List[Expr], _loc: Location)
+    extends Stmt(_loc) {
+  val name = _name
+  val actuals = _actuals
+
+  override def pretty = ""
+}
+
+case class MethodCall(_fi: Field, _actuals: List[Expr], _loc: Location)
+    extends Stmt(_loc) {
+  val fi = _fi
+  val actuals = _actuals
+
+  override def pretty = ""
+}
+
+case class Return(_value: Option[Expr], _loc: Location)
+    extends Stmt(_loc) {
+  val value = _value
+
+  override def pretty = ""
+}
+
+case class If(_cond: Expr, _thn: Block, _els: Block, _loc: Location)
+    extends Stmt(_loc) {
+  val cond = _cond
+  val thn = _thn
+  val els = _els
+
+  override def pretty = ""
+}
+
+case class While(_cond: Expr, _body: Block, _loc: Location)
+    extends Stmt(_loc) {
+  val cond = _cond
+  val body = _body
+
+  override def pretty = ""
+}
+
+// @FIXME: is "extends Node" correct?
+case class Field(_expr: Expr, _name: String, _loc: Location)
+    extends Node(_loc) {
+  val expr = _expr
+  val name = _name
+
+  override def pretty = ""
+}
+
+abstract class Expr(_loc: Location) extends Node(_loc) {
 
 }
 
-case class GetField(value: Field)
-    extends Expr() {
+case class Id(_name: String, _loc: Location)
+    extends Expr(_loc) {
+  val name = _name
 
+  override def pretty = ""
 }
 
-case class ExprCall(name: String, actuals: List[Expr])
-    extends Expr() {
+case class GetField(_value: Field, _loc: Location)
+    extends Expr(_loc) {
+  val value = _value
 
+  override def pretty = ""
 }
 
-case class This()
-    extends Expr() {
+case class ExprCall(_name: String, _actuals: List[Expr], _loc: Location)
+    extends Expr(_loc) {
+  val name = _name
+  val actuals = _actuals
 
+  override def pretty = ""
 }
 
-case class New(ty: Type, actuals: List[Expr])
-    extends Expr() {
+case class This(_loc: Location)
+    extends Expr(_loc) {
 
+  override def pretty = ""
 }
 
-case class BExpr(op: String, left: Expr, right: Expr)
-    extends Expr() {
+case class New(_ty: Type, _actuals: List[Expr], _loc: Location)
+    extends Expr(_loc) {
+  val ty = _ty
+  val actuals = _actuals
 
+  override def pretty = ""
 }
 
-case class UExpr(op: String, value: Expr)
-    extends Expr() {
+case class BExpr(_op: String, _left: Expr, _right: Expr, _loc: Location)
+    extends Expr(_loc) {
+  val op = _op
+  val left = _left
+  val right = _right
 
+  override def pretty = ""
 }
 
-case class LitExpr(value: Literal)
-    extends Expr() {
+case class UExpr(_op: String, _value: Expr, _loc: Location)
+    extends Expr(_loc) {
+  val op = _op
+  val value = _value
 
+  override def pretty = ""
 }
 
-abstract class Literal() {}
+case class LitExpr(_value: Literal, _loc: Location)
+    extends Expr(_loc) {
+  val value = _value
 
-case class IntLit(value: Int)
-  extends Literal() {}
+  override def pretty = ""
+}
 
-case class BoolLit(value: Boolean)
-  extends Literal() {}
+abstract class Literal(_loc: Location) extends Node(_loc) {}
 
-case class StringLit(value: String)
-  extends Literal() {}
+case class IntLit(_value: Int, _loc: Location)
+    extends Literal(_loc) {
+  val value = _value
+
+  override def pretty = ""
+}
+
+case class BoolLit(_value: Boolean, _loc: Location)
+    extends Literal(_loc) {
+  val value = _value
+
+  override def pretty = ""
+}
+
+case class StringLit(_value: String, _loc: Location)
+    extends Literal(_loc) {
+  val value = _value
+
+  override def pretty = ""
+}
 
