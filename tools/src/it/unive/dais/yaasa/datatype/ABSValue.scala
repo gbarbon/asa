0f3f0e1933356932ff000a01c5bc0221d1cb89f9
gbarbon <gianluca.barbon@gmail.com>
2016-01-10 20:50:32 +0100

    _

--- tools/src/it/unive/dais/yaasa/datatype/ABSValue.scala
+++ tools/src/it/unive/dais/yaasa/datatype/ABSValue.scala
@@ -4,6 +4,7 @@ import it.unive.dais.yaasa.datatype.widening_lattice._
 
 /**
  * @author gbarbon
+ * @author esteffin
  */
 object ABSValue {
 
@@ -15,7 +16,6 @@ object ABSValue {
     def !=^(sndVal: AbsBoolean[AbsValue]): AbsBoolean[AbsValue]
     def notAt: AbsBoolean[AbsValue]
 
-    //TODO: Strongly suggested
     def containsTrue: Boolean
     def containsFalse: Boolean
 
@@ -28,7 +28,7 @@ object ABSValue {
     override def toString() = pretty
   }
   trait AbsBooleanFactory[AbsValue] extends WideningLatticeFactory[AbsValue] {
-    def fromBool(b: Boolean): AbsBoolean[AbsValue]
+    def fromBool(value: Boolean): AbsBoolean[AbsValue]
     def sTrueAt: AbsBoolean[AbsValue]
     def sFalseAt: AbsBoolean[AbsValue]
     //Note: top, bottom are inherited by WideningLatticeFactory
@@ -57,10 +57,10 @@ object ABSValue {
     override def toString() = pretty
   }
   trait AbsIntegerFactory[AbsValue] extends WideningLatticeFactory[AbsValue] {
-    def fromNum(b: Int): AbsInteger[AbsValue]
-    def interval(a: Int, b: Int): AbsInteger[AbsValue]
-    def open_left(a: Int): AbsInteger[AbsValue]
-    def open_right(b: Int): AbsInteger[AbsValue]
+    def fromNum(value: Int): AbsInteger[AbsValue]
+    def interval(left: Int, right: Int): AbsInteger[AbsValue]
+    def open_left(right: Int): AbsInteger[AbsValue]
+    def open_right(left: Int): AbsInteger[AbsValue]
     //Note: top, bottom are inherited by WideningLatticeFactory
   }
 
@@ -82,18 +82,12 @@ object ABSValue {
     override def toString() = pretty
   }
   trait AbsStringFactory[AbsValue] extends WideningLatticeFactory[AbsValue] {
-    def fromString(b: Int): AbsString[AbsValue]
+    def fromString(value: Int): AbsString[AbsValue]
     //Note: top, bottom are inherited by WideningLatticeFactory
   }
 
   trait AbstractValue {
-    val value: Any
-    // val ty: Type // @FIXME: referece to absyn, to avoid!
-
-    def join(secondEl: AbstractValue): AbstractValue
-    // @TODO: raise exception if types are not compatible
-
-    def pretty: String = "[%s]" format (value)
+    def pretty: String
     override def toString() = pretty
   }
 
@@ -102,12 +96,7 @@ object ABSValue {
   trait AbsStr extends AbstractValue with AbsString[AbstractValue]
 
   trait AbstractDegrValue {
-    val value: Any
-    // val ty: Type // @FIXME: referece to absyn, to avoid!
-
-    def join(sndVal: AbstractDegrValue): AbstractDegrValue
-
-    def pretty: String = "[%s]" format (value)
+    def pretty: String
     override def toString() = pretty
   }
 
