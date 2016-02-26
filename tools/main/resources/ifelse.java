047a5f208e16b08ac2b5de7ec476150038a15966
gbarbon <gianluca.barbon@gmail.com>
2016-02-26 10:02:42 +0100

    Union issue fixed

--- tools/main/resources/ifelse.java
+++ tools/main/resources/ifelse.java
@@ -4,15 +4,17 @@ class ifelse extends Activity {
         boolean booleanVal;
         int var;
         booleanVal = readlib.readBool("aBool");
-        var = 5; //readlib.readInt("anInt");
+        //var = 5; //readlib.readInt("anInt");
+        var = readlib.readPreciseInt();
         //log(booleanVal);
 
         if (booleanVal == true) {
-            var = var + 7;
+            //var = var + 7;
+            log(12);
         }
         else {
             var = var - 8;
         }
-        log(var);
+        //log(var);
     }
 }
